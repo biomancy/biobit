1bae8342b8d2a8ff581dd37151033d149fa52746
alnfedorov <18511496+alnfedorov@users.noreply.github.com>
2025-03-05 21:46:26 +0000

    Remove obsolete files

--- obsolete/rcount/source/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import adapters
-from .bam import SingleEndBAM, PairedEndBAM
-from .source import Source, AlignedBlocks
-
-__all__ = ["Source", "AlignedBlocks", "SingleEndBAM", "PairedEndBAM", "adapters"]
