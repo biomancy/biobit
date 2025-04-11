7771d59e548ecd5d858ba473f820269d0225f99e
alnfedorov <18511496+alnfedorov@users.noreply.github.com>
2025-04-11 11:11:52 +0100

    Add ByPyPointer wrapper for Python object types with pointer Hash/Eq

--- modules/core/py/src/bindings/utils/mod.rs
+++ modules/core/py/src/bindings/utils/mod.rs
@@ -1,5 +1,7 @@
+mod by_py_pointer;
 mod fallible_py_runtime;
 mod importable_py_module;
 
+pub use by_py_pointer::ByPyPointer;
 pub use fallible_py_runtime::{FallibleBorrowed, FallibleBound};
 pub use importable_py_module::ImportablePyModuleBuilder;
